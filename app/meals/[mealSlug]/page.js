8f0d5093e1cc5f1ac8f18c0911986018862e93d7
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2025-08-19 11:33:41 +0100

    Add 404 handling for missing meals
    Introduces a not-found page for meals and updates the meal details page to trigger a 404 when a meal is not found. Also adds minor UI improvements to the meal details page.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    return (
+      notFound() // If meal not found, trigger 404 page
+    );
+  }
   meal.instructions = meal.instructions.replace(/\n/g, "<br />"); // Remove HTML tags
   return (
     <>
@@ -13,7 +19,7 @@ export default function MealDetailsPage({ params }) {
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
-          <p>
+          <p className={classes.creator}>
             by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
           <p className={classes.summary}>{meal.summary}</p>
