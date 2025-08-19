df66ca98b210b8b5cc199e5b2abb2a3937361014
Benard Otokpa <137802427+BenardOtokpa@users.noreply.github.com>
2025-08-19 10:58:37 +0100

    Add meals feature with SQLite integration
    Introduces a meals feature with server-side data fetching using better-sqlite3. Adds error and loading states, a not-found page, and updates the meals page to fetch and display meals from a SQLite database. Includes initial dummy data, database initialization script, and required dependencies.

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+export default function ErrorPage() {
+
+  return (
+    <main className="error">
+      <h1>An Error Occurred!</h1>
+      <p>There was an error fetching the meals. Please try again later.</p>
+    </main>
+  );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,23 +1,34 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/mealsGrid";
-export default function MealsPage() {
-  return;
-  <>
-    <header className={classes.header}>
-      <h1>
-        Delicious meals, created {""}
-        <span className={classes.highlight}>by you</span>
-      </h1>
-      <p>
-        Choose your favorite recipe and cook it yourself. It is easy and fun{" "}
-      </p>
-      <p className={classes.cta}>
-        <Link href="/meals/share">share your favorite recipe</Link>
-      </p>
-    </header>
-    <main>
-      <MealsGrid meals={[]} />
-    </main>
-  </>;
+import { getAllMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import mealsLoadingPage from "./loadingState";
+async function Meals() {
+  const meals = await getAllMeals(); // Fetch meals data, simulating a delay
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created {""}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={mealsLoadingPage()}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
